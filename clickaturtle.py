39dc418abd6ddb1f1f1f7ea18d786d25723f7f36
Mcshin0920 <90926649+Mcshin0920@users.noreply.github.com>
2021-11-08 11:19:07 -0800

    Update clickaturtle.py

--- clickaturtle.py
+++ clickaturtle.py
@@ -2,11 +2,20 @@
 #-----import statements-----
 import turtle as trtl
 import random as rand
+import leaderboard as lb
 #-----game configuration----
 spotColor = "pink"
 spotShape = "circle"
 spotSize = 5
 points = 0
+font_setup = ("Arial", 20, "normal")
+timer = 30
+counter_interval = 1000 
+timer_up = False
+leaderboard_file_name = "a122_leaderboard.txt"
+leader_names_list = []
+leader_scores_list = []
+player_name = input ("Please enter your name:")
 #-----initialize turtle-----
 spot = trtl.Turtle()
 counter_one = trtl.Turtle()
@@ -16,15 +25,11 @@ spot.turtlesize(spotSize)
 spot.fillcolor(spotColor)
 spot.speed(0)
 spot.penup()
-font_setup = ("Arial", 20, "normal")
-timer = 30
-counter_interval = 1000   #1000 represents 1 second
-timer_up = False
 #-----game functions--------
 def spot_clicked(x,y):
   counter_one.clear()
   global points
-  size = rand.randint(1, 10)
+  size = rand.randint(1, 5)
   change_position(size)
   counter_one.color("white")
   counter_one.hideturtle()
@@ -47,7 +52,7 @@ def change_position(size):
     spot.goto(new_xpos,new_ypos)
     spot.showturtle()
   else:
-    spot.clear()
+    spot.hideturtle()
 
 
 def countdown():
@@ -62,11 +67,32 @@ def countdown():
   if timer <= 0:
     counter.write("Time's Up", font=font_setup)
     timer_up = True
+    manage_leaderboard
   else:
     counter.write("Timer: " + str(timer), font=font_setup)
     timer -= 1
     counter.getscreen().ontimer(countdown, counter_interval)
-    
+  
+
+# manages the leaderboard for top 5 scorers
+def manage_leaderboard():
+  
+  global leader_scores_list
+  global leader_names_list
+  global points
+  global spot
+
+  # load all the leaderboard records into the lists
+  lb.load_leaderboard(leaderboard_file_name, leader_names_list, leader_scores_list)
+
+  # TODO
+  if (len(leader_scores_list) < 5 or points > leader_scores_list[4]):
+    lb.update_leaderboard(leaderboard_file_name, leader_names_list, leader_scores_list, player_name, points)
+    lb.draw_leaderboard(leader_names_list, leader_scores_list, True, spot, points)
+
+  else:
+    lb.draw_leaderboard(leader_names_list, leader_scores_list, False, spot, points)
+
 #-----events----------------
 def start_game():
   wn = trtl.Screen()
